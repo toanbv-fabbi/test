d00335d980409375beb2ea2c6d7e698fddb25d90
Tung Ngo <tungngo@coccoc.com>
2020-10-13 13:25:27 +0700

    Initial Commit

---
+++ NewsFeed/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  NewsFeed
+//
+//  Created by tungngo on 10/13/20.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ NewsFeed/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  NewsFeed
+//
+//  Created by tungngo on 10/13/20.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
